260e37acac83733221e10c1932b31a8bd44468ad
Shavez Khan <120683080+Shavezkhan0@users.noreply.github.com>
2025-03-26 18:11:04 +0530

    Delete scr directory

--- scr/pages/api/pincode.js
+++
@@ -1,9 +0,0 @@
-// src/pages/api/hello.js
-
-export default function handler(req, res) {
-  const pincode = {
-    110025: { city: "South Delhi", state: "New Delhi", country: "Inda" },
-  };
-
-  res.status(200).json(pincode);
-}
